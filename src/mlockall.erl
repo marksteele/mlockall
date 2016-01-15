987ff13b989a009bb82d2246bea32f079e48cd48
Mark Steele <mark@control-alt-del.org>
2016-01-15 15:01:10 -0500

    Dynamically loading so based on env for escript

--- src/mlockall.erl
+++ src/mlockall.erl
@@ -22,24 +22,20 @@
 -on_load(init/0).
 
 init() ->
-    SoName = case code:priv_dir(?MODULE) of
-                 {error, bad_name} ->
-                     case filelib:is_dir(filename:join(["..", "priv"])) of
-                         true ->
-                             filename:join(["..", "priv", "mlockall_nif"]);
-                         false ->
-                             filename:join(["priv", "mlockall_nif"])
-                     end;
-                 Dir ->
-                     filename:join(Dir, "mlockall_nif")
-             end,
-    erlang:load_nif(SoName, 0),
-    case erlang:system_info(otp_release) of
-        "R13B03" ->
-            true;
-        _ ->
-            ok
-    end.
+  Path = case application:get_env(code,sopath) of
+           {ok, CodePath} ->
+             CodePath;
+           _ ->
+             case code:priv_dir(?MODULE) of
+               {error, _} ->
+                 EbinDir = filename:dirname(code:which(?MODULE)),
+                 AppPath = filename:dirname(EbinDir),
+                 filename:join(AppPath, "priv");
+               CodePath ->
+                 CodePath
+             end
+         end,
+  erlang:load_nif(filename:join(Path, ?MODULE), 0).
 
 lock(_Flags) ->
     erlang:nif_error(mlockall_nif_not_loaded).
